8584dec5ab0787a56c46fa2e465c801d27a30960
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-12-01 16:06:54 -0800

    adding inputs to editor

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -2,5 +2,37 @@ import React from 'react';
 import './Editor.css';
 
 export default function Editor() {
-  return <p>This is the editor.</p>;
+  return (
+    <>
+      <div className="form-control">
+        <label>Top</label>
+        <select>
+          <option value="top-1">One</option>
+          <option value="top-2">Two</option>
+          <option value="top-3">Three</option>
+        </select>
+      </div>
+      <div className="form-control">
+        <label>Middle</label>
+        <select>
+          <option value="middle-1">One</option>
+          <option value="middle-2">Two</option>
+          <option value="middle-3">Three</option>
+        </select>
+      </div>
+      <div className="form-control">
+        <label>Bottom</label>
+        <select>
+          <option value="bottom-1">One</option>
+          <option value="bottom-2">Two</option>
+          <option value="bottom-3">Three</option>
+        </select>
+      </div>
+      <div className="form-control">
+        <label>Add a Catchphrase!</label>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+    </>
+  );
 }
